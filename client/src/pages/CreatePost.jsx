65e860f4eda8f91b0e6a3990ce37cf4fdaf376b3
RazerAds <adunair123@gmail.com>
2023-07-23 14:22:01 +0530

    added backend API url

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -25,7 +25,7 @@ const CreatePost = () => {
       setLoading(true);
       
       try {
-        const response = await fetch('https://dalle-rzb4.onrender.com/api/v1/post',{
+        const response = await fetch('https://imgen-qi1o.onrender.com/api/v1/post',{
           method: 'POST',
           headers: {
             "Content-Type": "application/json", 
@@ -48,7 +48,7 @@ const CreatePost = () => {
     if(form.prompt){
       try {
         setGeneratingImg(true);
-        const response = await fetch('https://dalle-rzb4.onrender.com/api/v1/dalle',{
+        const response = await fetch('https://imgen-qi1o.onrender.com/api/v1/dalle',{
           method: 'POST',
           headers: {
             "Content-Type": "application/json",
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -27,7 +27,7 @@ const Home = () => {
     const fetchPosts = async () => {
       setLoading(true);
       try {
-        const response = await fetch('https://dalle-rzb4.onrender.com/api/v1/post', {
+        const response = await fetch('https://imgen-qi1o.onrender.com/api/v1/post', {
           method: 'GET',
           headers: {
           "Content-Type": "application/json",
